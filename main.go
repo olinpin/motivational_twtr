89fb6832a85d4b8aa0ffb63caf44c7d957dda563
Oliver <Oliver.Hnat@gmail.com>
2022-07-15 20:07:43 +0200

    Changed time again

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ type Credentials struct {
 }
 
 func main() {
-	if time.Now().Hour()%4 == 1 || time.Now().Hour() == 0 {
+	if time.Now().Hour()%4 == 2 || time.Now().Hour() == 0 {
 		fmt.Print("running")
 		run()
 	}
